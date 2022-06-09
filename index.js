35509551900b6be615a815cc1602801c8ca008d8
chrstno040420 <104917222+chrstno040420@users.noreply.github.com>
2022-06-09 15:08:46 -0500

    commit tree b16e2abb99a4cfd220f9256e3ca4ef8c2c31b843

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+
+function saturdayFun(fun = "roller-skate"){
+    return `This Saturday, I want to ${fun}!`;
+}
+
+function mondayWork(work = "go to the office"){
+    return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(hard = "special"){ 
+    console.log(`You are ${hard}a hard worker${hard}!`);
+    
+}
+
